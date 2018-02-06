f70516b1c972d8c5785c0ba41613bfbc4d95972a
BobbyJoeSmith3 <lakota.sapa@gmail.com>
2018-02-06 11:02:54 -0500

    Access height and width values from form and log to console

--- designs.js
+++ designs.js
@@ -1,10 +1,26 @@
 // Select color input
 // Select size input
+let canvasHeight = document.getElementById('inputHeight').value;
+let canvasWidth = document.getElementById('inputWidth').value;
 
 // When size is submitted by the user, call makeGrid()
+
+document.getElementById('submitButton').onclick = makeGrid;
+
+function getGridHeight() {
+  canvasHeight = document.getElementById('inputHeight').value;
+  return canvasHeight;
+}
+
+function getGridWidth() {
+  canvasWidth = document.getElementById('inputWidth').value;
+  return canvasWidth;
+}
 
 function makeGrid() {
 
 // Your code goes here!
-
+  getGridHeight();
+  getGridWidth();
+  console.log('height: ' + canvasHeight + ', width: ' + canvasWidth);
 }
